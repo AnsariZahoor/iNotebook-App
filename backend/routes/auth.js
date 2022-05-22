e98dc8a42aee873133511d98e347bf61e7c9ff67
Ansari Zahoor <ansarizahoor7@gmail.com>
2022-05-22 20:31:04 +0530

    Users Validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
-router.post('/createuser', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], (req, res) => {
+
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {res.json({error:"Sorry a user with this email already exists", message:err.message})});
+
 })
 
 module.exports = router
